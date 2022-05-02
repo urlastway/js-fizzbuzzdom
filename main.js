6764c6deec91312bcb4e4fc0bfc08008c462a3fa
urlastway <andriy.semenchenko13@gmail.com>
2022-05-02 19:48:36 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+
+
+for (let i=1; i < 101; i++){
+    if ((i%3==0) && (i%5==0)){
+
+    console.log("FizBuzz");
+    
+    }
+
+    else if (i%5==0){
+
+    console.log("Buzz");
+
+    }
+
+    else if (i%3==0){
+
+        console.log("Fiz");
+    
+        }
+
+    else{
+        console.log(i)
+    }
+};
