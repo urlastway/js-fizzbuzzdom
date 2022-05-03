9fee5e8693da2df509960d2ac5b147dbcb172ad0
urlastway <andriy.semenchenko13@gmail.com>
2022-05-03 10:38:38 +0200

    com

--- main.js
+++ main.js
@@ -1,25 +1,36 @@
+const list = document.querySelector(".list")
 
 
-for (let i=1; i < 101; i++){
-    if ((i%3==0) && (i%5==0)){
-
-    console.log("FizBuzz");
+for (let i=1; i < 101; i++)
+    {
     
+    const item = document.createElement("li");
+    item.classList.add("item");
+
+    if ((i%3==0) && (i%5==0)){
+    console.log("FizzBuzz");
+    item.innerText = "FizzBuzz";
     }
 
     else if (i%5==0){
 
     console.log("Buzz");
+    item.innerText = "Buzz";
 
     }
 
     else if (i%3==0){
 
-        console.log("Fiz");
-    
-        }
-
-    else{
-        console.log(i)
+    console.log("Fizz");
+    item.innerText = "Fizz";
+        
     }
+
+    else {
+
+    console.log(i)
+    item.innerText = i;
+    }
+
+    list.append(item);
 };
